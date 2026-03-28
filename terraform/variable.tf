11828205fcd3b35ed94454e5c3afe1bd3f8bf910
beingsai <60880008+beingsai@users.noreply.github.com>
2026-03-28 23:45:42 +0530

    Add warning comment to key_name variable default

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -13,5 +13,5 @@ variable "instance_type" {
 variable "key_name" {
   description = "Name of the existing EC2 Key Pair"
   type        = string
-  default     = "saiawsdem0-account-keypair"
+  default     = "saiawsdem0-account-keypair"   # ⚠️ NO .pem
 }
